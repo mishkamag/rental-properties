6616295d36444aa3de63ba509873b9bf1634b217
mishkamag <m93.maglaperidze@gmail.com>
2024-03-01 22:56:41 +0400

    feat: impliment responsive navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "@/assets/styles/global.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "Find The Perfect Rental",
@@ -10,6 +11,7 @@ const MainLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
+        <Navbar />
         <main>{children}</main>
       </body>
     </html>
---
+++ app/properties/page.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const PropertiesPage = () => {
+  return (
+    <div>
+      <h1>Welcome PropertiesPage </h1>
+      <Link href="/">Go back home</Link>
+    </div>
+  );
+};
+
+export default PropertiesPage;
