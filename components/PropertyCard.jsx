b57d44f9807705d37135b3e50a08a60765c465fd
mishkamag <m93.maglaperidze@gmail.com>
2024-03-04 02:13:30 +0400

    fix: fix icons bug

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
+import {
+  FaBed,
+  FaBath,
+  FaRulerCombined,
+  FaMoneyBill,
+  FaMapMarker,
+} from "react-icons/fa";
 
 const PropertyCard = ({ property }) => {
   const getRateDisplay = () => {
@@ -34,15 +41,16 @@ const PropertyCard = ({ property }) => {
 
         <div className="flex justify-center gap-4 text-gray-500 mb-4">
           <p>
-            <i className="fa-solid fa-bed"></i> {property.beds}
+            <FaBed className="inline mr-2" />
+            {property.beds}
             <span className="md:hidden lg:inline">Beds</span>
           </p>
           <p>
-            <i className="fa-solid fa-bath"></i> {property.baths}
+            <FaBath className="inline mr-2" /> {property.baths}
             <span className="md:hidden lg:inline">Baths</span>
           </p>
           <p>
-            <i className="fa-solid fa-ruler-combined"></i>
+            <FaRulerCombined className="inline mr-2" />
             {property.square_feet}
             <span className="md:hidden lg:inline">sqft</span>
           </p>
@@ -51,19 +59,19 @@ const PropertyCard = ({ property }) => {
         <div className="flex justify-center gap-4 text-green-900 text-sm mb-4">
           {property.rates.nightly && (
             <p>
-              <i className="fa-solid fa-money-bill"></i> Nightly
+              <FaMoneyBill className="inline mr-2" /> Nightly
             </p>
           )}
 
           {property.rates.monthly && (
             <p>
-              <i className="fa-solid fa-money-bill"></i> Monthly
+              <FaMoneyBill className="inline mr-2" /> Monthly
             </p>
           )}
 
           {property.rates.weekly && (
             <p>
-              <i className="fa-solid fa-money-bill"></i> Weekly
+              <FaMoneyBill className="inline mr-2" /> Weekly
             </p>
           )}
         </div>
@@ -71,10 +79,9 @@ const PropertyCard = ({ property }) => {
         <div className="border border-gray-100 mb-5"></div>
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
-          <div className="flex align-middle gap-2 mb-4 lg:mb-0">
-            <i className="fa-solid fa-location-dot text-lg text-orange-700"></i>
-            <span className="text-orange-700">
-              {" "}
+          <div className="flex items-center	  gap-2 mb-4 lg:mb-0 text-orange-700">
+            <FaMapMarker className="inline mr-2" />
+            <span className="">
               {property.location.city} {property.location.state}{" "}
             </span>
           </div>
