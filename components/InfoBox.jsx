6a709f20f26d5dc6d87ca787373a58a96e59eee0
mishkamag <m93.maglaperidze@gmail.com>
2024-03-03 23:11:37 +0400

    feat: add info boxes section

---
+++ components/InfoBox.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+const InfoBox = ({
+  heading,
+  backgroundColor = "bg-gray-100",
+  textColor = "text-gray-800",
+  buttonInfo,
+  title,
+}) => {
+  return (
+    <section className={`${backgroundColor} p-6 rounded-lg shadow-md`}>
+      <h2 className={`${textColor} text-2xl font-bold`}>{heading}</h2>
+      <p className={`${textColor} mt-2 mb-4`}>{title}</p>
+      <Link
+        href={buttonInfo.link}
+        className={`${buttonInfo.backgroundColor} inline-block  text-white rounded-lg px-4 py-2 hover:opacity-80`}
+      >
+        {buttonInfo.text}
+      </Link>
+    </section>
+  );
+};
+
+export default InfoBox;
